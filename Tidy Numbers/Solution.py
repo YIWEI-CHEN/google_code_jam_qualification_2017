7ef3a98df60a4c1fbe0e2302ac6f075aef632778
Yi-Wei Chen <feberium@gmail.com>
2018-04-04 19:29:47 +0800

    I extended has_bigger function into while loop.
    1. When I need to check the same object again, I can use while True, until some condition is met.
    2. has_bigger function is more easy to read than extending the function, but I just did it for practice.

--- Tidy Numbers/Solution.py
+++ Tidy Numbers/Solution.py
@@ -6,18 +6,16 @@ Created on Wed Apr  4 17:00:24 2018
 @author: yiwei
 """
 
-def has_bigger(n):
-	for i in range(len(n) - 1):
-		if n[i] > n[i + 1]:
-			return i
-	return None
-
 for c in range(1,int(input())+1):
 	n = input()
 	tidy = n
 	
 	while True:
-		big = has_bigger(tidy)
+		big = None
+		for i in range(len(tidy) - 1):
+			if tidy[i] > tidy[i + 1]:
+				big = i
+				break
 		if big is None:
 			break
 		tidy = tidy[:big] + str(int(tidy[big]) - 1) + ''.join('9' for _ in tidy[big + 1: ])
