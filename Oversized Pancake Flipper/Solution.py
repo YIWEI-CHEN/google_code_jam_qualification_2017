d07e132caa86624734241159b5eec620b934f7fa
Yi-Wei Chen <feberium@gmail.com>
2018-04-04 16:28:30 +0800

    Rewrite my solution by the ideas from oop
    1. oop used bit manipulation and XOR to solve the problem
    	(a) Mapping s '-' -> 1 and '+' -> 0
    	(b) flip -> 1 * k
    	(c) each iteration,  s binary value XOR  (flip binary value  << remaining s length)
    2. I used '{:b}'.format(s) to get binary value length
    3. I used map and join function to map '-' and '+' to 1 and 0

--- Oversized Pancake Flipper/Solution.py
+++ Oversized Pancake Flipper/Solution.py
@@ -9,34 +9,16 @@ Created on Tue Apr  3 16:24:48 2018
 for i in range(1,int(input())+1):
 	s, k = input().split()
 	k = int(k)
-	neg_w = -1 * (k + 1)
-	pos_w = 1
-	s = map(lambda p: pos_w if p == '+' else neg_w, s)
+	s = ''.join(map(lambda p: '0' if p == '+' else '1', s))
+	s = int(s, 2)
+	x = int('1' * k, 2)
 	t = 0
-	state = 0
 
-	for p in s:
-		if state > 0 and p < 0:
-			state = p
-		else:
-			state += p
-
-		if state == k * neg_w:
-			t += 1
-			state = 0
-			continue
-
-		if state == neg_w + pos_w * (k - 1) + neg_w:
-			state = 0
-			t += 2
-			continue
-
-		if state == neg_w + pos_w + neg_w:
+	while s:
+		s_len = len('{:b}'.format(s))
+		if s_len - k < 0:
+			t = 'IMPOSSIBLE'
 			break
-
-		if state == neg_w + pos_w * k:
-			break
-
-	if state < 0:
-		t = 'IMPOSSIBLE'
+		s ^= x << (s_len - k)
+		t += 1
 	print('Case #{}: {}'.format(i, t))
